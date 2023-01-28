a07ce5d83c15d2b5443274d2484edd255ab309ea
Ruslan <devllartone@gmail.com>
2023-01-28 21:05:55 +0500

    Final work of gotyos

--- cmd/gotoys/main.go
+++ cmd/gotoys/main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/urfave/cli"
 )
@@ -18,7 +20,20 @@ func main() {
 			Name:  "mv",
 			Usage: "move file and fix imports",
 			Action: func(c *cli.Context) {
-				moveGo(c.Args().Get(0), c.Args().Get(1))
+				oldPath := c.Args().Get(0)
+				newPath := c.Args().Get(1)
+				moveGo(oldPath, newPath)
+
+				if strings.HasPrefix(oldPath, "./") {
+					oldPath = oldPath[2:]
+				}
+				if strings.HasPrefix(newPath, "./") {
+					newPath = newPath[2:]
+				}
+				files := getGoFilesList(".")
+				fmt.Println(files)
+
+				replaceInFile(files, oldPath, newPath)
 			},
 		},
 	}
@@ -27,13 +42,10 @@ func main() {
 }
 
 func moveGo(oldLocation, newLocation string) {
-	fmt.Println(oldLocation, newLocation)
-
 	err := os.Rename(oldLocation, newLocation)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func replaceInFile(listFiles []string, oldText, newText string) {
@@ -45,10 +57,35 @@ func replaceInFile(listFiles []string, oldText, newText string) {
 		}
 
 		output := bytes.ReplaceAll(input, []byte(oldText), []byte(newText))
+		fmt.Println(oldText, newText)
 
 		if err = ioutil.WriteFile(pathFile, output, 0666); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 	}
+}
+
+func getGoFilesList(path string) []string {
+	listFiles := []string{}
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			continue
+		}
+
+		if file.IsDir() {
+			listFiles = append(listFiles, getGoFilesList(path+"/"+file.Name())...)
+
+		} else if bool, err := regexp.Match(`.go`, []byte(file.Name())); err == nil && bool {
+			listFiles = append(listFiles, path+"/"+file.Name())
+		}
+	}
+
+	return listFiles
 }
