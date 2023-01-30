c0d54f0622551926ac4f40ba111a8f88b5d3fcd9
Ruslan <devllartone@gmail.com>
2023-01-30 18:48:55 +0500

    Although no, now the improvement is better

--- internal/commands/buy_drinks.go
+++ internal/commands/buy_drinks.go
@@ -42,10 +42,9 @@ func buyRandom() {
 			count := rand.Intn(int(state.Money/(20*price)+1)) + 1
 			sumPrice := price * float64(count)
 
-			totalSumMoreOneFifth := state.Money/sumPrice < 5
-			MoneyMoreOneDollarButSumPriceIsMore := state.Money > 1 && state.Money-sumPrice < 0
-			totalSumMoreOneTenthAndAlredyHaveDrink := state.Money/sumPrice < 10 && DrinkExistYet(drinkName, volume) != nil
-			if totalSumMoreOneFifth || MoneyMoreOneDollarButSumPriceIsMore || totalSumMoreOneTenthAndAlredyHaveDrink {
+			if state.Money/sumPrice < 5 ||
+				(state.Money > 1 && state.Money-sumPrice < 0) ||
+				(state.Money/sumPrice < 10 && DrinkExistYet(drinkName, volume) != nil) {
 				continue
 			}
 			go buyTransaction(drinkName, count, volume, price, state.RandomBuy)
