1d344c60bef05edd6835dbec9314f41498ea1d82
Ruslan <devllartone@gmail.com>
2023-01-30 20:13:20 +0500

    Added recipes

--- internal/drinks/coctails_data.go
+++ internal/drinks/coctails_data.go
@@ -7,12 +7,15 @@ var AviableCoctail = map[string]Coctail{
 		NecessaryTings: []string{"Бокал для вина", "Джиггер", "Коктейльная ложка", "Трубочки"},
 		Ingredients:    []string{"Апероль", "Просекко", "Содовая", "Апельсин", "Лёд в кубиках"},
 		Grammar:        []float64{0.100, 0.100, 0.020, 0.040, 0.060},
-		Instruction:    texts.InstCoctailAperrolShprits,
 		Description:    texts.DescCoctailAperrolShprits,
+		Instruction:    texts.InstCoctailAperrolShprits,
 	},
 
 	"Пина Колада": {
-		Ingredients: []string{"Белый ром", "Тёмный ром", "Кокосовый сироп", "Лаймовый сок", "Ананас", "Ананасовые листья", "Коктельная вишня крассная", "Дроблённый лёд"},
+		NecessaryTings: []string{"Харрикейн", "Джиггер", "Слайсер для ананаса", "Пресс для цитруссовых", "Совок для льда", "Трубочки", "Коктейльная шпажка", "Соковыжималка", "Блендер"},
+		Ingredients:    []string{"Белый ром", "Тёмный ром", "Кокосовый сироп", "Лаймовый сок", "Ананас", "Ананасовые листья", "Коктельная вишня крассная", "Дроблённый лёд"},
+		Grammar:        []float64{0.060, 0.015, 0.040, 0.020, 1, 1, 0.005, 0.060},
+		Instruction:    texts.InstCoctailPinaColada,
 	},
 
 	"Зомби": {
@@ -24,22 +27,43 @@ var AviableCoctail = map[string]Coctail{
 	},
 
 	"Ураган": {
-		Ingredients: []string{"Белый ром", "Тёмный ром", "Сироп маракуйи", "Гренандин", "Анансовый сок", "Апельсиновый сок", "Лаймовый сок", "Лимон", "Лёд в кубиках"},
+		NecessaryTings: []string{"Харрикейн", "Шейкер", "Стрейнер", "Джиггер", "Пресс для цитрусовых", "Трубочки"},
+		Ingredients:    []string{"Белый ром", "Тёмный ром", "Сироп маракуйи", "Гренандин", "Анансовый сок", "Апельсиновый сок", "Лаймовый сок", "Лимон", "Лёд в кубиках"},
+		Grammar:        []float64{0.030, 0.030, 0.015, 0.005, 0.060, 0.030, 0.020, 0.400},
+		Description:    texts.DescCoctailHurricane,
+		Instruction:    texts.InstCoctailHurricane,
+	},
+
+	"Ром кола": {
+		NecessaryTings: []string{"Хайбол", "Пресс для цитрусовых", "Коктейльная ложка", "Джиггер", "Трубочки"},
+		Ingredients:    []string{"Белый ром", "Лаймовый сок", "Кола", "Лайм", "Лёд в кубиках"},
+		Grammar:        []float64{0.050, 0.010, 0.140, 0.020, 0.180},
+		Description:    texts.DescCoctailRumCola,
+		Instruction:    texts.InstCoctailRumCola,
 	},
 
 	"Тёмный ром с колой": {
-		Ingredients: []string{"Тёмный ром", "Лаймовый сок", "Кола", "Лайм", "Лёд в кубиках"},
+		NecessaryTings: []string{"Хайбол", "Пресс для цитрусовых", "Коктейльная ложка", "Джиггер", "Трубочки"},
+		Ingredients:    []string{"Тёмный ром", "Лаймовый сок", "Кола", "Лайм", "Лёд в кубиках"},
+		Grammar:        []float64{0.050, 0.010, 0.140, 0.020, 0.180},
+		Description:    texts.DescCoctailDarkRumWithCola,
+		Instruction:    texts.InstCoctailDarkRumWithCola,
 	},
 
 	"Мохито": {
 		NecessaryTings: []string{"Хайлбол", "Мадлер", "Джиггер", "Коктейльная ложка", "Трубочка"},
 		Ingredients:    []string{"Белый ром", "Сахарный сироп", "Содовая", "Лайм", "Мята", "Дроблённый лёд"},
-		Instruction:    texts.InstCoctailMohito,
+		Grammar:        []float64{0.050, 0.015, 0.100, 0.080, 0.003, 0.200},
 		Description:    texts.DescCoctailMohito,
+		Instruction:    texts.InstCoctailMohito,
 	},
 
 	"Маргарита": {
-		Ingredients: []string{"Серебренная текила", "Трипл сек", "Сахарный сироп", "Лаймовый сок", "Лайм", "Соль", "Лёд в кубиках"},
+		NecessaryTings: []string{"Бокал маргарита", "Шейкер", "Стрейнер", "Джиггер", "Пресс для цитрусовых"},
+		Ingredients:    []string{"Серебренная текила", "Трипл сек Fruko Schulz", "Сахарный сироп", "Лаймовый сок", "Лайм", "Соль", "Лёд в кубиках"},
+		Grammar:        []float64{0.050, 0.025, 0.010, 0.030, 0.010, 0.002, 0.200},
+		Description:    texts.DescCoctailMargarita,
+		Instruction:    texts.InstCoctailMargarita,
 	},
 
 	"Негрони": {
