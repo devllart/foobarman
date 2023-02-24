eb30a45aa23877a3af9853c3de5728232ec3edb3
Ruslan <devllartone@gmail.com>
2023-02-24 10:38:52 +0500

    update names

--- internal/a_names/names.go
+++ internal/a_names/names.go
@@ -1,4 +1,7 @@
 package anames
 
+// const JsonDataDir = "data_json_for_prod"
 const JsonDataDir = "data_json"
+const GameDataDir = "internal/data/generated_data"
+
 const OutDirForGeneratedData = "internal/data"
